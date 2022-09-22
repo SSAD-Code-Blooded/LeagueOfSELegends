776dcafa2fa9ea0f679892dcf7c9a02dd2c4488c
jovanhuang <jhtc98@hotmail.com>
2022-09-23 01:51:52 +0800

    Added bare skeletons for Profile Page & Leaderboard Page

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        playGame();
+    }
+
+    // public void displayProfile()
+    // {
+    //     SceneManager.LoadScene("Profile");
+    // }
+    
+    // public void displayLeaderboard()
+    // {
+    //     SceneManager.LoadScene("Leaderboard");
+    // }
+
+    // public void CloseButton()
+    // {
+    //     SceneManager.LoadScene("StudentTeacherSelection");
+    // }
+
+
+
+}
