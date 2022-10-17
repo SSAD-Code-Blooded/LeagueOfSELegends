667f3e241756b7187df08b8a3552d2785edab9d6
Marcus Foo <43901768+marcusfooo@users.noreply.github.com>
2022-10-17 18:06:03 +0800

    Added menu

--- Assets/Scripts/TeleportToBattle.cs
+++ Assets/Scripts/TeleportToBattle.cs
@@ -7,7 +7,6 @@ public class TeleportToBattle : MonoBehaviour
 {
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("On point");
         SceneManager.LoadScene("Battle");       
     }
 
