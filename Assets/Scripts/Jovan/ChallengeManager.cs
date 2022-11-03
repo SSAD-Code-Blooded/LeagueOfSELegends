e2bc254d3e73c2efbc3817ef5c5776f77fa115ac
jovanhuang <jhtc98@hotmail.com>
2022-11-03 11:36:03 +0800

    Add comments and clean up code

--- Assets/Scripts/Jovan/ChallengeManager.cs
+++ Assets/Scripts/Jovan/ChallengeManager.cs
@@ -3,40 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// This class is used to manage the scene for Challenge's aspect.
+///
+/// It manages the loading of different scenes from Challenge scene.
 public class ChallengeManager : MonoBehaviour
 {
 
+    /// This method is called whenever user wants to go back to MainMenu button in Challenge Menu Scene.
+    ///
+    /// It brings the user back to Main Menu Scene.
     public void onClickBackToMainMenu()
     {
         SceneManager.LoadScene("3 Main Menu");
     }
 
+    /// This method is called whenever user clicks on back to Challenge Menu Scene.
+    ///
+    /// It manages the changing of button's colour when player click on it like turning it red when wrong answer is clicked.
     public void onClickBackToChallengeMenu()
     {
         SceneManager.LoadScene("4 Challenge Menu");
     }
-
-    public void onClickToCreateRoomMenu()
-    {
-        SceneManager.LoadScene("4.1 Challenge Create Room Menu");
-    }
-
-    
-    public void onClickToJoinMenu()
-    {
-            SceneManager.LoadScene("4.2 Challenge Join Room Menu");
-    }
-
-
-    public void onClickCreateRoom()
-    {
-        SceneManager.LoadScene("4.3 Challenge Room");
-    }
-
-    public void onClickJoinARoom()
-    {
-        SceneManager.LoadScene("4.3 Challenge Room");
-    }
-
 
 }
